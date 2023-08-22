904c9b1f965c06aa1e4be88990aee37e15a96cee
MJ Shelton <git@github.com>
2023-08-21 21:32:30 -0500

    Created model for request from client

---
+++ src/main/java/com/JSONpostroute/model/RequestJSON.java
@@ -0,0 +1,37 @@
+package com.JSONpostroute.model;
+
+public class RequestJSON {
+    private String product;
+    private int quantity;
+    private double unit_price;
+
+    public RequestJSON(String product, int quantity, double unit_price) {
+        this.product = product;
+        this.quantity = quantity;
+        this.unit_price = unit_price;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getUnit_price() {
+        return unit_price;
+    }
+
+    public void setUnit_price(double unit_price) {
+        this.unit_price = unit_price;
+    }
+}
