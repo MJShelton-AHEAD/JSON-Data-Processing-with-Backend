581ba53b56235c5498518476a66e590c8ebb1887
MJ Shelton <git@github.com>
2023-08-21 20:59:50 -0500

    Initial file structure

---
+++ src/main/java/com/JSONpostroute/JsonPostRouteApplication.java
@@ -0,0 +1,13 @@
+package com.JSONpostroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonPostRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonPostRouteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JSONpostroute/JsonPostRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JSONpostroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonPostRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
