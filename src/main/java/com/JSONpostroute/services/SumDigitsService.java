a12f39a7c8c62836584d3147b99b6cc643e0cde7
MJ Shelton <git@github.com>
2023-08-24 14:05:54 -0500

    Updated variable names for cleaner code

--- src/main/java/com/JSONpostroute/services/SumDigitsService.java
+++ src/main/java/com/JSONpostroute/services/SumDigitsService.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SumDigitsService {
-    public static int sumDigits(int totalOrders) {
-        if(totalOrders ==0)return 0;
-        return totalOrders%10 + sumDigits(totalOrders/10);
+    public static int sumDigits(int orderDigits) {
+        if(orderDigits ==0)return 0;
+        return orderDigits%10 + sumDigits(orderDigits/10);
     }
 }
