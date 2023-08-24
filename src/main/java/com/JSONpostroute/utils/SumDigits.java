8d92b9221520d42643fa2030b1c6085275847883
MJ Shelton <git@github.com>
2023-08-24 14:13:27 -0500

    Refactored spacing for cleaner code

--- src/main/java/com/JSONpostroute/utils/SumDigits.java
+++ src/main/java/com/JSONpostroute/utils/SumDigits.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class SumDigits {
     public static int sumDigits(int orderDigits) {
-        if(orderDigits ==0)return 0;
-        return orderDigits%10 + sumDigits(orderDigits/10);
+        if (orderDigits == 0) return 0;
+        return orderDigits % 10 + sumDigits(orderDigits / 10);
     }
 }
