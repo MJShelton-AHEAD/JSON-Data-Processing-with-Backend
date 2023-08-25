aae490801f12eff54f0d2c8b73671c267ae98e81
MJ Shelton <142437636+MJShelton-AHEAD@users.noreply.github.com>
2023-08-25 10:49:21 -0500

    Moved total order function to OrderFunction class (#4)
    * Moved total order function to OrderFunction class and made clearer variable
    
    * Refactored to save memory
    
    ---------
    
    Co-authored-by: MJ Shelton <git@github.com>

--- src/main/java/com/JSONpostroute/functions/orders/OrderFunctions.java
+++ src/main/java/com/JSONpostroute/functions/orders/OrderFunctions.java
@@ -1,4 +1,17 @@
 package com.JSONpostroute.functions.orders;
 
+import com.JSONpostroute.models.RequestJSON;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class OrderFunctions {
+    public static int totalOrders(List<RequestJSON> orderList){
+        int total_orders = 0;
+        for (int i = 0; i < orderList.size(); i++) {
+            total_orders += orderList.get(i).getQuantity();
+        }
+        return total_orders;
+    }
 }
