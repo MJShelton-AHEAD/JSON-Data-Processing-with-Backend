cc2c723bc1b1bc10d0d597b8594c5a22db538d4b
MJ Shelton <git@github.com>
2023-08-25 13:22:36 -0500

    Further code refactoring for simpler code

--- src/main/java/com/JSONpostroute/validations/ValidationFunctions.java
+++ src/main/java/com/JSONpostroute/validations/ValidationFunctions.java
@@ -2,9 +2,6 @@ package com.JSONpostroute.validations;
 
 public class ValidationFunctions {
     public static boolean reqHasOrdersValidation(String testString){
-        if(testString.contains("orders")){
-            return true;
-        }
-        return false;
+        return testString.contains("orders");
     }
 }
