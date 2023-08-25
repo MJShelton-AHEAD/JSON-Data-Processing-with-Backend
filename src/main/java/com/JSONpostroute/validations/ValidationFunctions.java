af5dd3f6d5be5d2af12910ba019ba13f88fea1bc
MJ Shelton <142437636+MJShelton-AHEAD@users.noreply.github.com>
2023-08-25 13:27:03 -0500

    Move order validation and refactor test (#6)
    * Move and refactor order validator into a validation class
    
    * Refactored for better validation test:
    
    * Further code refactoring for simpler code
    
    ---------
    
    Co-authored-by: MJ Shelton <git@github.com>

---
+++ src/main/java/com/JSONpostroute/validations/ValidationFunctions.java
@@ -0,0 +1,7 @@
+package com.JSONpostroute.validations;
+
+public class ValidationFunctions {
+    public static boolean reqHasOrdersValidation(String testString){
+        return testString.contains("orders");
+    }
+}
