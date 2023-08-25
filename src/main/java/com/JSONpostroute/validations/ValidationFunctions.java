f6a4b7e1c23e8bf3cfe9b8c5b0071838e9b60e9a
MJ Shelton <git@github.com>
2023-08-25 13:09:44 -0500

    Refactored for better validation test:

--- src/main/java/com/JSONpostroute/validations/ValidationFunctions.java
+++ src/main/java/com/JSONpostroute/validations/ValidationFunctions.java
@@ -2,7 +2,6 @@ package com.JSONpostroute.validations;
 
 public class ValidationFunctions {
     public static boolean reqHasOrdersValidation(String testString){
-        testString.toLowerCase();
         if(testString.contains("orders")){
             return true;
         }
